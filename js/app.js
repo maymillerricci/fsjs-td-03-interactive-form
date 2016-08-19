d73002ff65bdf67f45a27f994a934df09ab5d9ef
May Miller-Ricci <maymillerricci@gmail.com>
2016-08-19 12:42:05 -0700

    add activities interactivity: disable other options with same time slot display total cost of activities

--- js/app.js
+++ js/app.js
@@ -35,3 +35,57 @@ function selectFirstOfThemeIfHidden(colorValToSelect) {
     $("#color").val(colorValToSelect);
   }
 }
+
+// run functions on checking checkboxes in activities list
+$(".activities input").on("change", function() {
+  disableSameTimeActivities($(this));
+  displayTotalCost($(this));
+});
+
+// disable other activities that happen during the same time slot
+function disableSameTimeActivities(checkedCheckbox) {
+  if (checkedCheckbox.closest("label").text().match("day")) {
+    var timeSlot = getTimeSlotFromLabel(checkedCheckbox);
+    if (checkedCheckbox.closest("label").text().match(timeSlot)) {
+      if (checkedCheckbox.prop("checked")) {
+        $(".activities label:contains('" + timeSlot + "')").addClass("disabled");
+        $(".activities label:contains('" + timeSlot + "')").children("input").prop("disabled", true);
+        checkedCheckbox.closest("label").removeClass("disabled");
+        checkedCheckbox.prop("disabled", false);
+      } else {
+        $(".activities label:contains('" + timeSlot + "')").removeClass("disabled");
+        $(".activities label:contains('" + timeSlot + "')").children("input").prop("disabled", false);
+      }
+    }
+  }
+}
+
+// display total cost of all activities, if any activities are checked
+function displayTotalCost(checkedCheckbox) {
+  $(".total-cost").removeClass("is-hidden");
+  
+  var initialTotalCost = parseInt($(".total-cost span").text());
+  var costForActivity = parseInt(getCostFromLabel(checkedCheckbox));
+  var newTotalCost;
+
+  if (checkedCheckbox.prop("checked")) {
+    newTotalCost = initialTotalCost + costForActivity;
+  } else {
+    newTotalCost = initialTotalCost - costForActivity;
+  }
+  $(".total-cost span").text(newTotalCost);
+
+  if (newTotalCost === 0) {
+    $(".total-cost").addClass("is-hidden");
+  }
+}
+
+// pull out day and time for activity from full activity label
+function getTimeSlotFromLabel(checkedCheckbox) {
+  return checkedCheckbox.closest("label").text().split("—")[1].split(",")[0];
+}
+
+// pull out cost for activity from full activity label
+function getCostFromLabel(checkedCheckbox) {
+  return checkedCheckbox.closest("label").text().split("$")[1];
+}
